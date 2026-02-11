8c1a736ce3221726ed131d9bab9d5e903038abfb
Veristian <veristianjarvico2@gmail.com>
2026-02-11 15:42:16 +0800

    Boss

---
+++ Assets/Scripts/Boss/BossManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossManager : MonoBehaviour
+{
+    
+
+    //Boss Appeaar
+    //Boss Leave
+    //Boss Talk Counter
+    //Boss Dialog Spawner
+    //Boss Gives more works
+    //Boss detect ur listening
+    
+}
