ba980bee514dbbfc39df0db2485651bb77d12f46
Watanuki74 <jackcarlson1211@gmail.com>
2026-02-26 01:21:43 +0800

    WinScene, LoseScene, IntermissionScene Added. Setting ui update + button functional
    WinScene no ui yet, setting volume slider not yet

---
+++ Assets/Scripts/SettingMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingMenu : MonoBehaviour
+{
+    public GameObject settingMenu;
+    public void BackButton()
+    {
+        settingMenu.SetActive(false);
+    }
+
+    public void HomeButton()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("TestMenu");
+    }
+}
