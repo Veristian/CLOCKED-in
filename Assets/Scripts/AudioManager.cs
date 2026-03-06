03e24ce711c6fcb69d4aaf6942c801be6764279e
Watanuki74 <jackcarlson1211@gmail.com>
2026-03-06 14:38:28 +0800

    audio mixer

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    [SerializeField] AudioMixer mixer;
+    public const string MASTER_KEY = "Master Volume";
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        LoadVolume();
+    }
+
+    void LoadVolume()
+    {
+        float masterVolume = PlayerPrefs.GetFloat(MASTER_KEY, 1f);
+
+        mixer.SetFloat(AudioController.MIXER_MASTER, Mathf.Log10(masterVolume) * 20);
+    }
+}
