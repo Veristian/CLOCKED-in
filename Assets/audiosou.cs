1576a78f6ce163a3dd5cda21cacf5cbd26bee227
Watanuki74 <jackcarlson1211@gmail.com>
2026-03-12 14:27:30 +0800

    sfx updt

---
+++ Assets/audiosou.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class audiosou : MonoBehaviour
+{
+    [SerializeField] AudioMixer mixer;
+    [SerializeField] Slider masterVolume;
+
+    public const string MIXER_MASTER = "Master Volume";
+
+    void Awake()
+    {
+        masterVolume.onValueChanged.AddListener(SetMasterVolume);
+    }
+
+    void Start()
+    {
+        masterVolume.value = PlayerPrefs.GetFloat(AudioManager.MASTER_KEY, 1f);
+    }
+
+    void OnDisable()
+    {
+        PlayerPrefs.SetFloat(AudioManager.MASTER_KEY, masterVolume.value);
+    }
+
+    void SetMasterVolume(float value)
+    {
+        mixer.SetFloat(MIXER_MASTER, Mathf.Log10(value) * 20);
+    }
+}
+
