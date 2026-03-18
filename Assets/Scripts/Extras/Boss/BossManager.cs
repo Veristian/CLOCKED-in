e79b2ab2b039d487d2aec85d8440009e55703b29
nerfoutlaws <rizalmanrosman@gmail.com>
2026-03-18 15:26:58 +0800

    line randomizer ofr boss

--- Assets/Scripts/Extras/Boss/BossManager.cs
+++ Assets/Scripts/Extras/Boss/BossManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
- 
+
 [RequireComponent(typeof(AudioSource))]
 public class BossManager : MonoBehaviour
 {
@@ -24,7 +24,7 @@ public class BossManager : MonoBehaviour
             this.maxReentryInterval = maxReentryInterval;
         }
     }
-    public List<BossSettingPerLevel> bossSettingPerLevel = 
+    public List<BossSettingPerLevel> bossSettingPerLevel =
     new List<BossSettingPerLevel>
     {
         new BossSettingPerLevel(1,3, 20f, 30f),
@@ -36,14 +36,14 @@ public class BossManager : MonoBehaviour
     public float timeToBossAngry = 5f;
     public float timeToBossSatisfied = 10f;
     public float dialogChangeTime = 2f;
-    public List<string> bossConverstation;
+    public List<string> bossConverstation;  
     // public List<string> bossListenToMe;
     public string reprimandDialog = "Hey! Look at me when im talking to you";
     public string angryDialog = "Fine! If you don't want to listen, then just take more work!";
     bool startedDialog;
     float bossAngryTimer;
     // float bossSatisfiedTimer;  
-    float dialogTimer;  
+    float dialogTimer;
     string currentDialog;
     int currentDialogIndex;
     bool readyToContinue;
@@ -57,6 +57,8 @@ public class BossManager : MonoBehaviour
     [Header("References")]
     public TextMeshProUGUI bossDialogBox;
     public Animator bossAnimator;
+
+    private List<string> selectedConversations;   // holds the 6 random ones for this boss visit
 
     private void Start()
     {
@@ -92,6 +94,10 @@ public class BossManager : MonoBehaviour
     public void BossStart()
     {
         reentryTimer = UnityEngine.Random.Range(minReentryInterval, maxReentryInterval);
+
+        // Pick 6 random conversations every time the boss appears
+        selectedConversations = bossConverstation.OrderBy(x => UnityEngine.Random.value).Take(6).ToList();
+
         currentDialogIndex = 0;
         readyToContinue = true;
         startedDialog = true;
@@ -106,7 +112,7 @@ public class BossManager : MonoBehaviour
 
     }
     //Boss Talk Counter
-    
+
     //Boss Dialog Spawner
     void SetDialog(string dialog)
     {
@@ -119,7 +125,8 @@ public class BossManager : MonoBehaviour
         {
             if (bossAngryTimer > timeToBossAngry && dialogTimer < 0)
             {
-                if (AnimateDialog(angryDialog)) {
+                if (AnimateDialog(angryDialog))
+                {
                     bossAngryTimer = 0;
                 }
             }
@@ -131,22 +138,22 @@ public class BossManager : MonoBehaviour
         else
         {
 
-            if (!readyToContinue )//&& dialogTimer < 0)
+            if (!readyToContinue)//&& dialogTimer < 0)
             {
                 AnimateDialog(reprimandDialog);
             }
             else if (dialogTimer < 0)
             {
-                if (currentDialogIndex + 1 <= bossConverstation.Count)
+                if (currentDialogIndex + 1 <= selectedConversations.Count)
                 {
-                    if (AnimateDialog(bossConverstation[currentDialogIndex])) currentDialogIndex++;
+                    if (AnimateDialog(selectedConversations[currentDialogIndex])) currentDialogIndex++;
                 }
                 else
                 {
                     BossLeave();
                 }
             }
-                    
+
         }
         dialogTimer -= Time.deltaTime;
     }
@@ -158,13 +165,13 @@ public class BossManager : MonoBehaviour
 
         if (bossDialogBox.text != dialog)
         {
-            
+
             bossDialogBox.text = bossDialogBox.text + dialog[bossDialogBox.text.Length];
             audioSource.PlayOneShot(bossAudioClips[UnityEngine.Random.Range(0, bossAudioClips.Length)]);
         }
         else
         {
-            
+
             dialogTimer = dialogChangeTime;
             // readyToContinue = true;
             Debug.Log(bossDialogBox.text);
@@ -220,7 +227,7 @@ public class BossManager : MonoBehaviour
         {
             readyToContinue = false;
             Debug.Log("asdas");
-            currentDialogIndex = Mathf.Clamp(currentDialogIndex - 1, 0, bossConverstation.Count);
+            currentDialogIndex = Mathf.Clamp(currentDialogIndex - 1, 0, selectedConversations.Count);
         }
         else if (CheckAttention() && !readyToContinue)
         {
